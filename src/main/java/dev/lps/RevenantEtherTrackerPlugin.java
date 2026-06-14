9ef05d04c2f5c8b5f62145538e3af11a026ad6e5
Luis Pedroza-Soto <lps.bizdev@gmail.com>
2026-06-13 21:46:22 -0700

    Adds the ability for the custom tracker to sync with the collection log

--- src/main/java/dev/lps/RevenantEtherTrackerPlugin.java
+++ src/main/java/dev/lps/RevenantEtherTrackerPlugin.java
@@ -4,7 +4,12 @@ import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.Client;
+import net.runelite.api.ScriptID;
+import net.runelite.api.events.ScriptPostFired;
+import net.runelite.api.gameval.InterfaceID;
 import net.runelite.api.gameval.ItemID;
+import net.runelite.api.widgets.Widget;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.game.ItemStack;
@@ -18,6 +23,9 @@ import net.runelite.client.plugins.loottracker.LootReceived;
 )
 public class RevenantEtherTrackerPlugin extends Plugin
 {
+
+    @Inject
+    private Client client;
 
     @Inject
     private ConfigManager configManager;
@@ -44,7 +52,23 @@ public class RevenantEtherTrackerPlugin extends Plugin
         if (revenantEtherLooted > 0L)
         {
             totalRevenantEther += revenantEtherLooted;
-            configManager.setConfiguration("revenantEtherTracker", "totalEtherDropped", totalRevenantEther);
+            updateConfig();
+        }
+    }
+
+    @Subscribe
+    public void onScriptPostFire(ScriptPostFired event)
+    {
+        if (event.getScriptId() == ScriptID.COLLECTION_DRAW_LIST)
+        {
+            try
+            {
+                syncWithCollectionLog();
+            }
+            catch (IllegalStateException __)
+            {
+                // Do nothing.
+            }
         }
     }
 
@@ -64,5 +88,53 @@ public class RevenantEtherTrackerPlugin extends Plugin
     RevenantEtherTrackerConfig provideConfig(ConfigManager configManager)
     {
         return configManager.getConfig(RevenantEtherTrackerConfig.class);
+    }
+
+    /**
+     * Attempts to sync our custom revenant ether tracker with the number of revenant ether counted in the collection
+     * log.
+     *
+     * @implNote This function is reliant on the collection log interface being present, one way of ensuring this is to
+     *           listen for the `ScriptID.COLLECTION_DRAW_LIST` script to finish.
+     * @throws IllegalStateException When this method is invoked when the collection log interface is either not
+     *                               present or ready.
+     */
+    private void syncWithCollectionLog() throws IllegalStateException
+    {
+        /**
+         * Safety check: If our custom tracker is already past the 16-bit limit,
+         * we never need to trust or look at the collection log window ever again.
+         */
+        if (totalRevenantEther >= 65535)
+        {
+            return;
+        }
+
+        Widget collectionLogWindow = client.getWidget(InterfaceID.COLLECTION);
+        if (collectionLogWindow == null)
+        {
+            throw new IllegalStateException("This method must only be called after the collection log interface is ready!");
+        }
+
+        for (Widget item : collectionLogWindow.getDynamicChildren())
+        {
+            if (item.getItemId() != ItemID.WILD_CAVE_SHARD)
+            {
+                continue;
+            }
+
+            int collectionLogQuantity = item.getItemQuantity();
+            if (collectionLogQuantity > totalRevenantEther)
+            {
+                totalRevenantEther = collectionLogQuantity;
+                updateConfig();
+            }
+            break;
+        }
+    }
+
+    private void updateConfig()
+    {
+        configManager.setConfiguration("revenantEtherTracker", "totalRevenantEther", totalRevenantEther);
     }
 }
